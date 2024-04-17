b8edf8f134b8e2d1695d2638fed689a314e2a3ed
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-04-18 00:32:49 +0200

    Routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1> e.dashboard </h1>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1> } />
+        <Route path="/add" element={<h1>AddProduct</h1>} />
+        <Route path="/update" element={<h1>UpdateProduct</h1> } />
+        <Route path="/logout" element={<h1>Logout</h1> } />
+        <Route path="/profile" element={<h1>Profile</h1> } />
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ front-end/src/Navbar.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div>
+      <ul className="nav-ul">
+        <li>
+         <Link to="/" > Home</Link>
+        </li>
+        <li>
+          <Link to="/add" > Add Product</Link>
+        </li>
+        <li>
+          <Link to="/update" > Update Product</Link>
+        </li>
+        <li>
+          <Link to="/logout" > Logout</Link>
+        </li>
+        <li>
+          <Link to="/profile" > Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Navbar;
