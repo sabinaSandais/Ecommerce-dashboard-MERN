0a1ec9ac723e7bf1c49bf501812651d1cdf797c8
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-04-19 00:13:08 +0200

    coonected db

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("App is working");
-});
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/e-com");
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("product", productSchema);
+  const data = await product.find();
+  console.warn(data);
+};
+connectDB();
 app.listen(3000);
