6111748fae77f6f7ddf469e935c7a11df57f36ff
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-04-18 11:30:53 +0200

    Sign up page

--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const collectData = () => {
+    const data = { name, email, password };
+    console.log(data);
+  };
   return (
-    <div>
+    <div className="register">
       <h1>Register</h1>
+      <input
+        className="inputBox"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter name"
+      />
+      <input
+        className="inputBox"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter Email"
+      />
+      <input
+        className="inputBox"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter Password"
+      />
+      <button onClick={collectData} className="appButton" type="button">
+        Sign Up
+      </button>
     </div>
   );
 };
